fab0209258e404cce3ce94eae0843dd7e888eed2
MUHAMMED ALI AYAS <ali.ayas@eroglu.corp>
2025-08-05 16:08:55 +0300

    feat: Add SOAP integration and JSON export functionality

---
+++ ProductSoapService/Dtos/ProductQueryFilterDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductSoapService.Dtos;
+
+public class ProductQueryFilterDTO
+{
+    public int? id { get; set; }
+    public string? barkod { get; set; }
+    public string? itemId { get; set; }
+    public string? renk { get; set; }
+    public string? beden { get; set; }
+    public decimal?  price { get; set; }
+}
